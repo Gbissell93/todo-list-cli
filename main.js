c5343022d2aee5b34ead135f3677decc739170c1
Gbissell93 <gregorybisselledu@gmail.com>
2021-06-14 15:39:13 -0400

    input working, needs more work

---
+++ main.js
@@ -0,0 +1,19 @@
+const prompt = require("prompt-sync")();
+let todoList = [];
+let todoItem = "";
+let itemCount = 0;
+
+let loop = true; //loop condition
+while(loop === true) {
+    console.log(`\n ${todoList} \n`)
+    console.log("\nWhat would you like to do? \n \n[1] Create a new item \n \n[2]  update item\n \n[3] delete an item\n")  // user option
+    const option = prompt("[4] exit app") //moved prompt to last line of text to avoid loop repeat on key stroke
+
+    if (option == 1) {
+        itemCount++
+        const newItem = prompt("\nenter a new item\n") //new item input
+        todoList.push(`${itemCount}. [Incomplete] ${newItem}`)
+    }
+
+
+}
