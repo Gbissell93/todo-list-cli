fd8bd4849196bc83bf1822246f38dea15ef34552
Gbissell93 <gregorybisselledu@gmail.com>
2021-06-14 15:40:21 -0400

    input working, spacing added

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let loop = true; //loop condition
 while(loop === true) {
     console.log(`\n ${todoList} \n`)
     console.log("\nWhat would you like to do? \n \n[1] Create a new item \n \n[2]  update item\n \n[3] delete an item\n")  // user option
-    const option = prompt("[4] exit app") //moved prompt to last line of text to avoid loop repeat on key stroke
+    const option = prompt("[4] exit app \n") //moved prompt to last line of text to avoid loop repeat on key stroke
 
     if (option == 1) {
         itemCount++
