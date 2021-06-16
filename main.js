2c7eff847ac043e36376e66215236bd5a6cca2d2
Gbissell93 <gregorybisselledu@gmail.com>
2021-06-16 09:53:36 -0400

    finished update and remove section

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ while(loop === true) {
         itemCount++
         const newItem = prompt("enter new item: ") //new item input
         console.log("") //empty string for space
-        todoList.push(`${itemCount}. [Incomplete] ${newItem}`)
+        todoList.push(`${todoList.length + 1}. [Incomplete] ${newItem}`)
         }
 
 
@@ -38,24 +38,32 @@ while(loop === true) {
         else if (option == 2) {
             const update = prompt("please select an item to update: "); //item update prompt
             const updateSelect = todoList[Number(update - 1)] //item selection
+            let choice = update - 1;
             console.log(`\nyou've selected ${updateSelect}`)
             console.log("\n [1] to complete ")
             const updateOpt = prompt(" [2] to remove ")
+            console.log(updateSelect)
             
             // update options =====================================================
+            //complete
+            if (updateOpt === '1') { 
+                let completed = todoList.splice(choice, 1).toString().replace(`[Incomplete]`, `[Complete]`);
 
-            if (updateOpt === '1') {               
-                todoList.push(`${itemCount}. [Complete] ${updateSelect}`)
-                todoList.splice(updateSelect - 1, 1)  
+                todoList.splice(choice, 0, completed)
             }
-            
-
-            
-
+            //remove
+            if (updateOpt === '2') { 
+                let deleted = todoList.splice(choice, 1)
+            itemCount--
         }
     
-    else {
+
+    }
+
+    if (option == 0) {
         loop = false
     }
 
+   
+    
 }
